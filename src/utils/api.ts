33f675a3b694b11c404d8727dbe45044f254a907
Steven Ko <steven.ko@shoplineapp.com>
2024-11-30 14:45:53 +0800

    feat(env): change name

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { AxiosInstance } from 'axios';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+const API_KEY = process.env.API_KEY;
 
 function createApiInstance(): AxiosInstance {
   const api = axios.create({
