37e6314164f3be215874260aa80fb7d1b1eea50a
Steven Ko <steven.ko@shoplineapp.com>
2024-12-01 20:45:45 +0800

    feat(mocks/movie): add mockMovies

--- src/__test__/components/MovieList/index.test.tsx
+++ src/__test__/components/MovieList/index.test.tsx
@@ -2,21 +2,7 @@ import { render, screen } from '@testing-library/react';
 import MovieList from '../../../components/MovieList';
 import type { MovieListProps } from '../../../components/MovieList';
 import { BrowserRouter } from 'react-router-dom';
-
-const mockMovies = [
-  {
-    id: 1,
-    title: 'Test Movie',
-    poster_path: '/test-path.jpg',
-    release_date: '2023-03-20',
-  },
-  {
-    id: 2,
-    title: 'Test Movie 2',
-    poster_path: null,
-    release_date: '2024-01-01',
-  },
-];
+import { mockMovies } from '../../../mocks/movie';
 
 const renderMovieList = (props: MovieListProps) => {
   return render(
@@ -34,19 +20,19 @@ describe('MovieList', () => {
       loadMore: jest.fn(),
     });
 
-    expect(screen.getByText('Test Movie')).toBeInTheDocument();
-    expect(screen.getByText('Test Movie 2')).toBeInTheDocument();
-    expect(screen.getByText('2023')).toBeInTheDocument();
-    expect(screen.getByText('2024')).toBeInTheDocument();
+    expect(screen.getByText('Venom: Let There Be Carnage')).toBeInTheDocument();
+    expect(screen.getByText('Carmen 1945')).toBeInTheDocument();
+    expect(screen.getByText('2021')).toBeInTheDocument();
+    expect(screen.getByText('1988')).toBeInTheDocument();
 
     const images = screen.getAllByRole('img');
     expect(images[0]).toHaveAttribute(
       'src',
-      'https://image.tmdb.org/t/p/w500/test-path.jpg'
+      'https://image.tmdb.org/t/p/w500/1MJNcPZy46hIy2CmSqOeru0yr5C.jpg'
     );
     expect(images[1]).toHaveAttribute(
       'src',
-      'https://via.placeholder.com/500x750?text=No+Image'
+      'https://image.tmdb.org/t/p/w500/raNbLci95MJ3L3dMnG7L7bD37lY.jpg'
     );
   });
 
---
+++ src/mocks/movie.ts
@@ -0,0 +1,16 @@
+import type { Movie } from '../types/movie';
+
+export const mockMovies: Movie[] = [
+  {
+    id: 580489,
+    poster_path: '/1MJNcPZy46hIy2CmSqOeru0yr5C.jpg',
+    release_date: '2021-09-30',
+    title: 'Venom: Let There Be Carnage',
+  },
+  {
+    id: 383634,
+    poster_path: '/raNbLci95MJ3L3dMnG7L7bD37lY.jpg',
+    release_date: '1988-04-09',
+    title: 'Carmen 1945',
+  },
+];
