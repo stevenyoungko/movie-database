9c3466b2aa3ec3a60a49e3d96c8aec2478b19487
Steven Ko <steven.ko@shoplineapp.com>
2024-12-01 21:42:33 +0800

    feat(test): add test ReviewSection

---
+++ src/__test__/components/MovieDetail/ReviewSection.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import ReviewSection from '../../../components/MovieDetail/ReviewSection';
+import { mockMovieDetail } from '../../../mocks/movie';
+
+describe('ReviewSection', () => {
+  it('should render reviews correctly', () => {
+    render(<ReviewSection reviews={mockMovieDetail.reviews.results} />);
+
+    expect(screen.getByText('評論')).toBeInTheDocument();
+    expect(screen.getByText('garethmb')).toBeInTheDocument();
+    expect(
+      screen.getByText(/When audiences last saw Eddie Brock/)
+    ).toBeInTheDocument();
+    expect(screen.getByText('2021/9/30')).toBeInTheDocument();
+  });
+
+  it('should show no reviews message when reviews array is empty', () => {
+    render(<ReviewSection reviews={[]} />);
+
+    expect(screen.getByText('目前沒有評論')).toBeInTheDocument();
+  });
+});
